047d7c6b7f3550033ac2ccbe4a415edaa8adcf20
Yuri Kruk <96660042+yuriverso@users.noreply.github.com>
2023-02-03 09:25:23 -0300

    Removed ultimate tabs functions and simplified the rest

--- GetCifras.py
+++ GetCifras.py
@@ -3,11 +3,6 @@ from bs4 import BeautifulSoup
 from os import listdir
 
 
-"""
-make functions to download from cifras and ultimate tabs
-"""
-
-
 def makeRequest(songName, google=False, bing=False):
     if google:
         radical = "https://www.google.com/search?q="
@@ -23,67 +18,34 @@ def makeRequest(songName, google=False, bing=False):
 
 # Function to search the song name at google.com
 def googleSearch(songName):
-    attempt = 1
-    while attempt <= 2:
-        if attempt == 1:
-            songName += ' cifra'
     
-            soup = makeRequest(songName, google=True)
-            for a in soup.findAll('a'):
-                if "cifraclub.com.br" in a.get("href") or "cifras.com.br" in a.get("href"):
-                    return "https://"+'/'.join(a.get("href").split("//")[1].split('/')[:-1])
-                
-        elif attempt == 2:
-            songName += " tab"
-
-            soup = makeRequest(songName, google=True)
-            for a in soup.findAll('a'):
-                if "ultimate-guitar.com" in a.get("href"):
-                    return "https://"+'/'.join(a.get("href").split("//")[1].split('/')[:-1])
-                
-        attempt += 1
+    songName += ' cifra'
+    
+    soup = makeRequest(songName, google=True)
+    for a in soup.findAll('a'):
+        if "cifraclub.com.br" in a.get("href") or "cifras.com.br" in a.get("href"):
+            return "https://"+'/'.join(a.get("href").split("//")[1].split('/')[:-1])
+                      
     print("No song found. ")
     return None
 
 
 # Function to search the song name at bing.com
 def bingSearch(songName):
-    attempt = 1
-    while attempt <= 2:
-        if attempt == 1:
-            songName += ' cifra'
-    
-            soup = makeRequest(songName, bing=True)
-            for li in soup.find("ol").findAll("li"):
-                if "cifraclub" in li.find('a').get("href") or "cifras" in li.find('a').get("href"):
-                    return li.find('a').get("href")
-                
-        elif attempt == 2:
-            songName += " tab"
 
-            soup = makeRequest(songName, bing=True)
-            for li in soup.find("ol").findAll("li"):
-                if "ultimate-guitar.com" in li.find('a').get("href"):
-                    return li.find('a').get("href")
+    songName += ' cifra'
+    
+    soup = makeRequest(songName, bing=True)
+    for li in soup.find("ol").findAll("li"):
+        if "cifraclub" in li.find('a').get("href") or "cifras" in li.find('a').get("href"):
+            return li.find('a').get("href")
                 
-        attempt += 1
     print("No song found. ")
     return None
 
-    """radical = "https://www.bing.com/search?q="
-    searchUrl = radical + songName.replace(' ', '+')
-    print("")
-    r = requests.get(searchUrl)
-    soup = BeautifulSoup(r.text, "html.parser")
-    results = soup.findAll("li", {"class": "b_algo"})
-    for result in results:
-        if "cifraclub" in result.h2.a.get("href"):
-            return result.h2.a.get("href")
-    return None"""
-
 
 # Function to access the found url and download the tab
-def cifraclubDownload(songName, url):
+def downloadTab(songName, url):
     if " cifra" in songName:
         songName = songName.replace(" cifra", "")
         
@@ -99,7 +61,7 @@ def cifraclubDownload(songName, url):
     with open("cifras/"+songName+".txt", 'w', encoding = "utf-8") as f:
         f.write(musicTab)
     print("\nTab downloaded!\n")
-
+    
 
 def main():
     while True:
@@ -113,25 +75,17 @@ def main():
         else:
             # google search
             search = None
-            #search = googleSearch(songName)
+            search = googleSearch(songName)
             if search == None:
                 print("Not found at google.com, trying another approach...")
                 # bing search
                 search = bingSearch(songName)
-            if "cifraclub" in search:
-                cifraclubDownload(songName, search)
-            elif "cifras" in search:
-                pass
-            elif "ultimate" in search:
-                pass
-            
-            
-            
-            #url = search(a)
-            #download(a, url) if url != None else print("\nSong not found! \n")
-            
+            if "cifraclub" in search or "cifras" in search:
+                downloadTab(songName, search)
+
 
 if __name__ == "__main__":
     main()
+    
 
 
